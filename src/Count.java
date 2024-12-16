cc77fea1cd82e575205e0010d3ae994987796e16
matheus <matheus.pereira@zup.com.br>
2024-12-16 18:42:47 -0300

    initial commit

---
+++ src/Count.java
@@ -0,0 +1,9 @@
+public class Count {
+    int number1;
+    int number2;
+
+    public int soma(){
+        int valor = number1 + number2;
+        return  valor;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Count count = new Count();
+
+        count.number1 = 10;
+        count.number2 = 12;
+        System.out.println(count.soma());
+    }
+}
